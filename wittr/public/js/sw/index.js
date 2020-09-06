cb55b46ba49cd90fdadd9317651dbae5f186e4be
64J0 <vinigaio97@gmail.com>
2020-09-06 02:43:53 -0300

    Adding a cache strategy to serve static files when the network is offline

--- wittr/public/js/sw/index.js
+++ wittr/public/js/sw/index.js
@@ -1,8 +1,34 @@
-self.addEventListener("fetch", function (event) {
+const urlsToCache = [
+  '/',
+  'js/main.js',
+  'css/main.css',
+  'imgs/icon.png',
+  'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff'
+];
 
-  if (event.request.url.endsWith('.jpg')) {
-    event.respondWith(
-      fetch('/imgs/dr-evil.gif')
-    );
-  }
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    // TODO: open a cache named 'wittr-static-v1'
+    // Add cache the urls from urlsToCache
+    caches.open('wittr-static-v1')
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  // TODO: respond with an entry from the cache if there is one.
+  // If there isn't, fetch from the network
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) return response;
+
+        return fetch(event.request);
+      })
+      .catch(function () {
+        return new Response("Uh oh, that totally failed!");
+      })
+  );
 });
