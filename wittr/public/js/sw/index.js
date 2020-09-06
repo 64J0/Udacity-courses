5cbe9814177a41b38a7dd511d8e3c8befd115e3f
64J0 <vinigaio97@gmail.com>
2020-09-05 23:29:51 -0300

    Responding a HTML file with the service worker

--- wittr/public/js/sw/index.js
+++ wittr/public/js/sw/index.js
@@ -1,7 +1,11 @@
 self.addEventListener("fetch", function (event) {
   event.respondWith(
-    new Response("Hello <b>world</b>", {
-      headers: { "Content-Type": "text/html; charset=utf-8" }
-    })
+    new Response(
+      "<p>Hello world</p>" +
+      "<br />" +
+      "<p class='a-winner-is-me'>Me</p>",
+      {
+        headers: { "Content-Type": "text/html; charset=utf-8" }
+      })
   );
 });
